60c5321fd96be20e7279047c537266b21a5075c0
khalidtaha1999 <74780502+khalidtaha1999@users.noreply.github.com>
2023-11-02 00:22:03 +0300

    fixes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,15 +1,19 @@
 <header id="header" class="header d-flex align-items-center fixed-top">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-evenly">
+    <div class="container-fluid container-xl d-flex align-items-center mobile_nav justify-content-between" >
+
 
         <a href="/{{Config::get('app.locale')}}" class="logo d-flex align-items-center">
             <!-- Uncomment the line below if you also wish to use an image logo -->
-             <img src="{{asset('frontend/img/header_logo.png')}}"   alt="">
+            <img src="{{asset('frontend/img/header_logo.png')}}"   alt="">
+
+            <span class="text-white small mr-2 logo-dis-login">QUSAI AL MALLAH CONSULTING OFFICE<br><span>Accounting, administrative & Tax</span></span>
+
         </a>
 
         <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
         <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
 
-        <nav id="navbar" class="navbar" >
+        <nav id="navbar" class="navbar">
             <ul>
                 <li><a href="/{{Config::get('app.locale')}}" class="active">{{__('general.home')}}</a></li>
                 <li><a href="/{{Config::get('app.locale')}}/about-us">{{__('general.aboutUs')}}</a></li>
